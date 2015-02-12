029d2a26cf1c38b99d102a5f4053c8e038087c76
Namgon Kim <day10000@gmail.com>
2015-02-13 03:09:48 +0900

    Add keepalived-ovs and keepalived-bridge that includes scripts preparinng keepalived configuration

---
+++ keepalived-ovs/init_node_SERVER-MSTER.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+sudo ifconfig eth1 192.168.0.252/24 up	
+
+sudo apt-get update
+sudo apt-get install -y keepalived
+
+sudo cat > /etc/keepalived/keepalived.conf << EOF
+global_defs {
+  router_id haproxy1
+}
+
+vrrp_script haproxy {
+  script "killall -0 haproxy"
+  interval 2
+  weight 2
+}
+
+vrrp_instance 50 {
+  state MASTER
+  interface eth1
+
+  virtual_router_id 50
+  priority 101
+  advert_int 1
+
+  virtual_ipaddress {
+    192.168.0.254
+  }
+
+  track_script {
+    haproxy
+  }
+}
+EOF
+
+sudo sysctl -w net.ipv4.ip_nonlocal_bind=1
+sudo service keepalived start
