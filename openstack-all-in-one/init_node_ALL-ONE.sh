d807775f6a8c0c6f5a44fb61738122508dbfeccd
Namgon Kim <day10000@gmail.com>
2015-02-17 15:34:03 +0900

    Update openstack-all-in-one to use local apt-cacher

--- openstack-all-in-one/init_node_ALL-ONE.sh
+++ openstack-all-in-one/init_node_ALL-ONE.sh
@@ -2,6 +2,15 @@
 
 sudo ifconfig eth1 10.0.0.101/24 up
 
+echo "1. configure apt-get proxy"
+CACHE_SERVER="211.224.204.145:23142"
+sudo cat > /etc/apt/apt.conf.d/02proxy <<EOF
+Acquire::http { Proxy "http://$CACHE_SERVER"; };
+EOF
+
+echo "2. apt-get update"
+sudo apt-get update
+
 #sudo apt-get install -y vlan iperf
 #sudo modprobe 8021q
 #sudo ifconfig eth2 up
