5ad37b5e2e5e342b4f2aae0158a4a470b70ea3a3
Namgon Kim <day10000@gmail.com>
2015-02-25 03:38:21 +0900

    * EXT_GROUPS에 정의된 내용을 바탕으로 새로운 group을 생성   - push_vlan과 set_vlan_vid를 엮어서 사용하는 데 시간을 많이 소모 * FLOW_RULES   - 기존의 "in_port dl_vlan group" 구조에서   - "match action" 구조로 수정 (dl_vlan이 없는 경우도 cover하기 위해서)

--- openstack-mn-1c/init_node_AGGR-SW.sh
+++ openstack-mn-1c/init_node_AGGR-SW.sh
@@ -8,15 +8,18 @@
 # * open vswitch를 이용하여 스위칭
 #   - inport, vlan_id기반으로 outport를 결정  
 #   - 추가적인 action이 필요한 경우를 위해 EXT_GROUPS정의
-#
+#   - (중요) 
+#    VLAN이 있는 flow와 VLAN이 없는 flow에 대해서 동일한 FLOW_RULE을 이용하기 위해
+#    FLOW_RULE배열을 match와 action이 빈 칸으로 구분되는 형태로 변경
 ###########################################################################
 
 BR0="br-aggr"
 BR0_ITFS=(  "eth1"  "eth2"  "eth3"  "eth4"  "eth5"  "eth6"  "eth7")
 
-# the array index of EXT_GROUPS will be used for group id 
-EXT_GROUPS[16]="push_vlan:0x8100,set_field:10-\>vlan_vid,output:6"
-EXT_GROUPS[17]="strip_vlan,output:7"
+# BASE_GROUP_ID + Array index will be used for group id 
+BASE_GROUP_ID=16
+EXT_GROUPS[0]="push_vlan:0x8100,set_vlan_vid:10,output:6"
+EXT_GROUPS[1]="strip_vlan,output:7"
 
 # - SVC (VLAN 3000~4000) - All-in-one과 C-Node-1만 통신하므로 VLAN 구분없이 연결
 #    . in_port:em1 -> output:em2   #All-in-one => C-Node-1
@@ -31,17 +34,17 @@ FLOW_RULES[2]="in_port=2 group=1"
 #    * Server Farm 트래픽 (VLAN 10)
 #        . in_port:em3,vlan=10 -> output: em4   # All-in-one ==> C-Node-1
 #	     . in_port:em4,vlan=10 -> output: em3   # C-Node-1 ==> All-in-one
-FLOW_RULES[3]="in_port=3 dl_vlan=11 group=4"
-FLOW_RULES[4]="in_port=4 dl_vlan=11 group=3"
-FLOW_RULES[5]="in_port=3 dl_vlan=10 group=4"
-FLOW_RULES[6]="in_port=4 dl_vlan=10 group=3"
+FLOW_RULES[3]="in_port=3,dl_vlan=11 group=4"
+FLOW_RULES[4]="in_port=4,dl_vlan=11 group=3"
+FLOW_RULES[5]="in_port=3,dl_vlan=10 group=4"
+FLOW_RULES[6]="in_port=4,dl_vlan=10 group=3"
 
 #- WAN (VLAN 10~2000) - 각 고객별로 고객이 지닌 모든 WAN용 VLAN에 대해 아래 룰을 반복 입력
 #      . in_port:em6,vlan=10 -> strip_vlan,output: em7                                     # All-in-one ==> WAN (VLAN 태그 제거후 전달)
 #      . in_port:em7 -> push_vlan:0x8100,set_field:10-\>vlan_vid,output: em6   # WAN ==> All-in-one (VLAN 태그 추가후 전달)
 # 추가로 strip_vlan을 하는 group과 vlan_vid를 추가하는 group을 새로 지정
-FLOW_RULES[7]="in_port=6 dl_vlan=10 group=17"
-FLOW_RULES[8]="in_port=7 dl_vlan=10 group=16"
+FLOW_RULES[7]="in_port=6,dl_vlan=10 group=17"
+FLOW_RULES[8]="in_port=7,dl_vlan=10 group=16"
 
 ###########################################################################
 
@@ -98,27 +101,30 @@ add_groups() {
 
 ext_groups() {
 	BR_NAME=$1
-	declare -a GROUPS=("${!2}")
-	
-	for grp_id in ${!GROUPS[@]}; do
-		action=${GROUPS[$grp_id]}
-		add_group $BR_NAME $grp_id "all" $action	
-	done
-	
+    declare -a GRPS=("${!2}")
+    base_idx=$3
+    
+    if [ -z $base_idx ]; then base_idx=0; fi
+    
+    for arr_idx in ${!GRPS[@]}; do
+    	action=${GRPS[$arr_idx]}
+    	
+    	grp_id=$((arr_idx + base_idx))
+        add_group $BR_NAME $grp_id "all" $action
+    done
 }
 
 add_flow() {
 	BR_NAME=$1
-	IN_PORT=$2
-	DL_VLAN=$3
-	GRP_ID=$4
-	TBL_ID=$5
-	
-	if [ -z $TBL_ID ]; then TBL_ID=0; fi
-	
-	cmd="sudo ovs-ofctl -O OpenFlow13 add-flow $BR_NAME \
-			 table=$TBL_ID,$IN_PORT,$DL_VLAN,actions=$GRP_ID"
-	run_commands $cmd
+    MATCH_=$2
+    ACTION=$3
+    TBL_ID=$4   
+    
+    if [ -z $TBL_ID ]; then TBL_ID=0; fi
+
+    cmd="sudo ovs-ofctl -O OpenFlow13 add-flow $BR_NAME \
+             table=$TBL_ID,$MATCH_,actions=$ACTION"
+    run_commands $cmd
 }
 
 add_flows() {
@@ -128,14 +134,15 @@ add_flows() {
 	# use ${!FLOW_RULE[@]} instead of ${FLOW_RULE[@]}
 	# ${FLOW_RULE[@]}를 사용하면 bash가 빈칸으로 구분된 문자열을 배열 element로 인식  
 	for idx in ${!FLOW_RULE[@]}; do
-		flow=${FLOW_RULE[$idx]}
-		
-		in_port=`echo $flow | awk '{print $1}'`
-		dl_vlan=`echo $flow | awk '{print $2}'`
-		grp_id_=`echo $flow | awk '{print $3}'`
-		
-		add_flow $BR_NAME $in_port $dl_vlan $grp_id_	
-	done
+		rule=${FLOW_RULE[$idx]}
+        
+        echo "rule= $rule"
+        match_=`echo $rule | awk '{print $1}'`
+        action=`echo $rule | awk '{print $2}'`
+        
+        echo "add_flow $BR_NAME $match_ $action"
+        add_flow $BR_NAME $match_ $action			
+	done		
 }
 
 dump_groups() {
@@ -154,7 +161,7 @@ dump_flows() {
 
 init_ovs    $BR0 BR0_ITFS[@]
 add_groups  $BR0 BR0_ITFS[@]
-ext_groups  $BR0 EXT_GROUPS[@] 
+ext_groups  $BR0 EXT_GROUPS[@] $BASE_GROUP_ID 
 add_flows   $BR0 FLOW_RULES[@]
 dump_groups $BR0
 dump_flows  $BR0
