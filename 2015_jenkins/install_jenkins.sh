84b928c8681b9c0df5f3e0319f59c2aeeaf1e9dd
Namgon Kim <ng.kim@kt.com>
2015-09-17 22:02:38 +0900

    Test for jenkins

---
+++ 2015_jenkins/install_jenkins.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+wget -q -O - https://jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
+sudo sh -c 'echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt-get update
+sudo apt-get install -y jenkins
+
+# needed to install gitlab hook plugin
+sudo apt-get install -y ruby-full git
