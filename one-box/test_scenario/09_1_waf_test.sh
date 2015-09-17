df04f6b4dacb7e4fbf4ce0fee80bf802c6936d63
Namgon Kim <day10000@gmail.com>
2015-09-17 14:57:35 +0900

    WAF API call script - add debug method that prints function name with messages

--- one-box/test_scenario/09_1_waf_test.sh
+++ one-box/test_scenario/09_1_waf_test.sh
@@ -3,19 +3,29 @@
 source "./00_check_config.sh"
 
 NS="qrouter-fbbba21e-718b-4d68-96cd-cdafd3fed71a"
-cmd="ip netns exec $NS curl --insecure -H \"Expect:\" -vX POST https://192.168.10.9/webapi/auth -d 'id=admin&password=wafpenta!23'"
-#run_commands_return $cmd
-#echo $RET
+WAF_IP="192.168.10.17"
+DEBUG=1
 
-#echo $SESS_ID
-#ip netns exec $NS curl --insecure -H "Expect:" -vX POST https://192.168.10.9/webapi/auth -d 'id=admin&password=wafpenta!23'
-#curl --insecure -H "Content-Type:application/x-www-form-urlencoded" -b $KEY -vX GET https://192.168.10.9/webapi/conf/network_interface
+debug_msg() {
+  msg="$*"
+  if [ ! -z $DEBUG ]; then 
+    print_msg "\n$msg"
+  fi
+}
 
-KEY="WP_SESSID=0q543t2epghato8nh9tlvpk706"
+get_session_id() {
+  debug_msg "${FUNCNAME}: request"
+  local RET=`ip netns exec $NS curl --insecure -H \"Expect:\" -vX POST https://${WAF_IP}/webapi/auth -d 'id=admin&password=wafpenta!23' 2>&1 | awk '/WP_SESSID/{print \$3}'`
+  debug_msg "${FUNCNAME}: $RET"
+
+  KEY=${RET:0:36}
+}
+
 
 waf_get_call() {
   URL=$1
-  ip netns exec $NS curl --insecure -H "Content-Type:application/x-www-form-urlencoded" -b $KEY -vX GET $URL
+  local cmd="ip netns exec $NS curl --insecure -H "Content-Type:application/x-www-form-urlencoded" -b $KEY -vX GET $URL 2>/dev/null"
+  run_commands_return $cmd
 }
 
 waf_put_call() {
@@ -23,26 +33,44 @@ waf_put_call() {
 	curl --insecure \
 		-b $KEY \
 		-H "Content-Type:application/x-www-form-urlencoded" \
-		-d '{"management_route":[{"nic":"eth0","gateway":"192.168.10.10","netmask":"255.255.255.128","dest_ip":"211.224.204.128"}]}' \
-		-vX PUT https://192.168.10.9/webapi/conf/management_route
+		-d '{"management_route":[{"nic":"eth0","gateway":"192.168.10.10","netmask":"0.0.0.0","dest_ip":"0.0.0.0"},{"nic":"eth0","gateway":"0.0.0.0","netmask":"255.255.255.0","dest_ip":"192.168.10.0"}]}' -vX PUT https://${WAF_IP}/webapi/conf/management_route
 }
 
-#send_command_to_waf https://192.168.10.9/webapi/conf/network_interface
-waf_get_call https://192.168.10.9/webapi/conf/management_route
+show_interfaces() {
+  waf_get_call https://${WAF_IP}/webapi/conf/network_interface
+  JSON=`echo $RET | tr -d '\n'` 
+  echo $JSON | json_reformat
+}
 
-echo ""
-echo ""
-echo ""
-echo ""
-echo ""
+show_management_route() {
+  waf_get_call https://${WAF_IP}/webapi/conf/management_route
+  echo $RET | json_reformat
+}
 
-waf_put_call 
+show_traffic() {
+  waf_get_call https://${WAF_IP}/webapi/statistics/system/traffic?stime=2015-09-08+01%3a24%3a55&etime=2015-09-08+01%3a29%3a55
+  #echo $RET | json_reformat
+  echo $RET
+  
+}
 
-echo ""
-echo ""
-echo ""
-echo ""
-echo ""
+show_cpu() {
+  local URL="https://${WAF_IP}/webapi/statistics/system/cpu_mem?stime=2015-09-08+01%3a24%3a55&etime=2015-09-08+01%3a29%3a55"
+  debug_msg "${FUNCNAME}: call $URL"
 
+  ip netns exec $NS curl --insecure -H \"Content-Type:application/x-www-form-urlencoded\" -b $KEY -vX GET $URL 2>/dev/null
+  debug_msg "${FUNCNAME}: done"
+}
 
-waf_get_call https://192.168.10.9/webapi/conf/management_route
+get_session_id
+#show_interfaces
+show_management_route
+show_cpu
+#show_traffic
+#echo ""
+
+#waf_put_call 
+
+#show_management_route
+
+#waf_get_call https://192.168.10.9/webapi/conf/management_route
