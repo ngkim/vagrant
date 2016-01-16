b86c292e14f489221e80fc4ba608770053aef857
Namgon Kim <day10000@gmail.com>
2016-01-17 02:12:49 +0900

    New directory for initialization of onebox - first job is registering VNF images

---
+++ one-box/01_init_onebox/00_check_config.sh
@@ -0,0 +1,14 @@
+WORK_HOME=/root/workspace/vagrant/one-box
+
+source "$WORK_HOME/config/default.cfg"
+source "$WORK_HOME/include/print_util.sh"
+source "$WORK_HOME/include/12_config.sh"
+source "$WORK_HOME/include/command_util.sh"
+
+source "./onebox.cfg"
+
+if [ -z ${OS_AUTH_URL+x} ]; then
+    source ~/admin-openrc.sh
+fi
+
+
---
+++ one-box/01_init_onebox/01_2_register_img.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+source "./00_check_config.sh"
+
+register_image() {
+  local _IMAGE_LABEL=$1
+  local _IMAGE_FILE=$2
+
+  if [ -f images/${_IMAGE_FILE} ]; then
+    cmd="glance image-create --name ${_IMAGE_LABEL} --disk-format $FILEFORMAT \
+        --container-format $CONTAINERFORMAT --visibility $ACCESSVALUE --progress --file images/${_IMAGE_FILE}"
+#        --container-format=$CONTAINERFORMAT --progress --file images/${_IMAGE_FILE}"
+    run_commands $cmd
+  fi
+}
+
+vnf_image_register() {
+  VNF_LABEL=$1
+  VNF_IMAGE=$2
+
+  echo "VNF_LABEL= ${VNF_LABEL}"
+  echo "VNF_IMAGE= ${VNF_IMAGE}"
+  if [ -f images/${VNF_IMAGE} ]; then
+    register_image $VNF_LABEL $VNF_IMAGE
+    sleep 3
+  fi
+}
+
+VNF_LABEL="${UTM_VNF_TYPE}-${UTM_NAME}-${UTM_VENDOR}-${UTM_VERSION}"
+VNF_IMAGE="${VNF_IMAGE_ROOT}/${UTM_VNF_TYPE}/${UTM_VENDOR}/images/${UTM_IMAGE}"
+vnf_image_register ${VNF_LABEL} ${VNF_IMAGE}
+
+VNF_LABEL="${WAF_VNF_TYPE}-${WAF_NAME}-${WAF_VENDOR}-${WAF_VERSION}"
+VNF_IMAGE="${VNF_IMAGE_ROOT}/${WAF_VNF_TYPE}/${WAF_VENDOR}/images/${WAF_IMAGE}"
+vnf_image_register ${VNF_LABEL} ${VNF_IMAGE}
+
+VNF_LABEL="${APC_VNF_TYPE}-${APC_NAME}-${APC_VENDOR}-${APC_VERSION}"
+VNF_IMAGE="${VNF_IMAGE_ROOT}/${APC_VNF_TYPE}/${APC_VENDOR}/images/${APC_IMAGE}"
+vnf_image_register ${VNF_LABEL} ${VNF_IMAGE}
